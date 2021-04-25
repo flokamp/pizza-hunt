3a16015c636533583323af61ea6179dc0dc2b803
flokamp <74511633+flokamp@users.noreply.github.com>
2021-04-25 16:35:32 -0500

    added reply functionality

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {
+	addComment,
+	removeComment,
+	addReply,
+	removeReply,
+} = require("../../controllers/comment-controller");
+
+module.exports = router;
+
+// /api/comments/<pizzaId>
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
